f670f95725712cdbd13f409fbf87ce12e5e174bc
Passkey-md <167205077+Passkey-md@users.noreply.github.com>
2024-06-27 08:54:19 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = "꧁༒☬☠ TERMINATOR☠︎☬༒꧂";
+global.owner = ['2348108395270'];
+global.premium = ['2348108395270'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
 global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
 global.sessionName = 'session';
